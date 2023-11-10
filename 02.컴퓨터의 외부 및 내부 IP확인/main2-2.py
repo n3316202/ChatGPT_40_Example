0b213cd5139f82dcbf911a12290ddbfdf5b7a5c3
tom <nqwrt@naver.com>
2023-11-10 19:04:12 +0900

    commit2

--- 02.컴퓨터의 외부 및 내부 IP확인/main2-2.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get('https://ipinfo.io/ip')
-external_ip = response.text.strip() #좌우열 문자공백제거
-
-print("외부 IP: " + external_ip)
--- 15.알림 프로그램 만들기/main15-1.py
+++
@@ -1,4 +0,0 @@
-from win10toast import ToastNotifier
-
-toaster = ToastNotifier()
-toaster.show_toast("제목", "내용")
